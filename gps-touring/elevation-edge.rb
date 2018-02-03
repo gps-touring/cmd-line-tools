394c7895c7a0a90efb9dbd2ab5dc9433c112c260
Matt Wallis <matt@parecco.co.uk>
2018-02-03 15:55:21 +0000

    Tidy up and simplify

--- gps-touring/elevation-edge.rb
+++ gps-touring/elevation-edge.rb
@@ -5,57 +5,51 @@ module GpsTouring
     include NetworkPointsArray
     attr_reader :logical_edge, :points, :metres
     def initialize(logical_edge, metres = 2)
+      # Create an elevationEdgecorresponding to the given LogicalEdge
+      # Each point in the created ElevationEdge will be taken from  the
+      # original set of NetworkPoints, but points with variations less than 'metres'
+      # will be omitted. 
       @logical_edge = logical_edge
       @metres = metres
-      point = logical_edge.from
-      link = logical_edge.first_link
-      @points = [point]
-      #$stderr.puts point
-      ele = point.ele
-      last_point_of_interest = point
-      #$stderr.puts ele
-      # Somewhat arbitrary initialization:
-      state = link.ele > point.ele ? :inc : :dec
+      original = OriginalEdge.new(logical_edge)
+      @points = [logical_edge.from]
+      local_min = original.points.first
+      local_max = original.points.first
 
-      while link.link_count == 2
-	next_ele = link.ele
+      # We're either in the process of increasing or decreasing in elevation
+      # - that's captured by the state variable.
+      # Somewhat arbitrary initialization:
+      state = original.points[1].ele > original.points[0].ele ? :inc : :dec
+
+      original.points.each do |p|
 	if state == :inc
-	  if next_ele > last_point_of_interest.ele
-	    last_point_of_interest = link
-	  end
-	  if next_ele >= @points.last.ele + metres
-	    add_point(link)
-	  end
-	  if next_ele <= last_point_of_interest.ele - metres
-	    add_point(last_point_of_interest) unless @points.last === last_point_of_interest
-	    add_point(link)
-	    last_point_of_interest = link
+	  if p.ele > local_max.ele
+	    local_max = p
+	    if p.ele >= @points.last.ele + metres
+	      add_point(p)
+	    end
+	  elsif p.ele <= local_max.ele - metres
+	    add_point(local_max) unless @points.last === local_max
+	    add_point(p)
 	    state = :dec
+	    local_min = p
 	  end
 	else
-	  if next_ele < last_point_of_interest.ele
-	    last_point_of_interest = link
-	  end
-	  if next_ele <= @points.last.ele - metres
-	    add_point(link)
-	  end
-	  if next_ele >= last_point_of_interest.ele + metres
-	    add_point(last_point_of_interest) unless @points.last === last_point_of_interest
-	    add_point(link)
-	    last_point_of_interest = link
+	  if p.ele < local_min.ele
+	    local_min = p
+	    if p.ele <= @points.last.ele - metres
+	      add_point(p)
+	    end
+	  elsif p.ele >= local_min.ele + metres
+	    add_point(local_min) unless @points.last === local_min
+	    add_point(p)
 	    state = :inc
+	    local_max = p
 	  end
 	end
-	next_links = link.links - [point]
-	raise "point is expected to be in link.links" unless next_links.size == 1
-	point = link
-	link = next_links.first
       end
-      add_point(link)
+      add_point(logical_edge.to) unless @points.last === logical_edge.to 
       @points.freeze
-      raise("Expected to start at beginning of LogicalEdge") unless @points.first === logical_edge.from
-      raise("Expected to reach end of LogicalEdge") unless @points.last === logical_edge.to
-      #$stderr.puts @points.map {|p| "(#{p.lat}, #{p.lon}, #{p.ele})"}.join(", ")
     end
     def add_point(p)
       raise("Algotithm error: adding same point twice in a row to an ElevationEdge: #{p}") if p === @points.last
