df74da6e665750ffd6df6da88b181e5c1bb392e1
Matt Wallis <matt@parecco.co.uk>
2018-03-04 19:50:00 +0000

    Computes acyclic paths in a LogicalGraph

--- gps_touring/lib/gps_touring/logical_graph.rb
+++ gps_touring/lib/gps_touring/logical_graph.rb
@@ -18,6 +18,39 @@ module GpsTouring
 	}
       }
     end
+    def node_edges
+      Hash[nodes.map {|n| 
+	[n, edges.find_all {|e| e.from === n}]
+      }]
+    end
+    def acyclic_paths
+      # Returns all paths through the graph that don't visit the same node twice.
+      # A path is a sequence of edges
+      edges.map {|e| grow([e])}.inject([], :+)
+    end
+    def grow(eseq)
+      # Returns an array of all of the acyclic paths that start with eseq
+      # including eseq itself
+
+      # We need to know which nodes we've already visited in the
+      # sequence of LogicalEdges called eseq:
+      already_visited = eseq.map {|e| e.from}
+
+      # Which edges lead on from  the end of eseq:
+      next_edges = node_edges[eseq.last.to]
+
+      # We're going to return the sequence of edges eseq ... 
+      res = [eseq]
+
+      # plus all of the sequneces that can be grown onto eseq
+      # without visiting the same node twice:
+      next_edges.
+	reject{|e| already_visited.include? e.to}.
+	each {|e| res += grow(eseq.dup.push(e)) }
+
+      res
+    end
+
     private
     def add_edges_from_node(node, node_test)
       unless @nodes.include? node
--- gps_touring/test/basic-route-finding.rb
+++ gps_touring/test/basic-route-finding.rb
@@ -22,7 +22,28 @@ class BasicRouteFinding < Test::Unit::TestCase
     #puts "logical_graphs: #{nwk.logical_graphs.size}"
     assert_equal(1, nwk.logical_graphs.size, "Graph should be connected")
 
-    pp logical_nodes.map {|n| n.name}
+    nwk.logical_graphs.each {|g|
+      g.node_edges.each{|n, es|
+	es.each{|e|
+	  assert_equal(n, e.from, "Edges from a node have their from attriute set to that same node")
+	}
+      }
+      puts "acyclic_paths: #{g.acyclic_paths.size}"
+      puts "nodes: #{g.nodes.size}"
+      puts "edges: #{g.edges.size}"
+
+      #pp g.acyclic_paths.map{|p| p.map {|e| e.object_id}}
+      g.acyclic_paths.each {|p|
+	(0...p.size-1).each {|i|
+	  assert_equal(p[i].to, p[i+1].from, "Sequences of edges in a path form a connected chain")
+	}
+      }
+      paths = g.acyclic_paths
+      assert_equal(paths.size, paths.map{|p| p.map{|e| e.object_id}}.sort.uniq.size, "No duplicate paths")
+    }
+
+
+    #pp logical_nodes.map {|n| n.name}
     assert_include(logical_nodes.map {|n| n.name}, "Cow Ark, Clitheroe", "Calling points should be logical nodes")
     assert_include(logical_nodes.map {|n| n.name}, "Newton-in-Bowland, Clitheroe", "Calling points should be logical nodes")
   end
