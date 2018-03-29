94b9816a5154a15cffd397da2f9b8b67cb070849
Matt Wallis <matt.wallis@tickpea.com>
2018-03-29 18:03:35 +0100

    Gear ratios tewak

--- ratios.rb
+++ ratios.rb
@@ -5,9 +5,11 @@
 #
 require 'pp'
 Rings, Sprockets = ARGV.map {|list| list.split(',').sort}.sort{|a,b| a.size <=> b.size}
-Wheel_diam = 27.0
+Wheel_diam = 27.0 #inches
+CmPerInch = 2.54
+Width = 8
+Rpm = 80
 class Gear
-    Width = 8
   attr_reader :ring, :sprocket
   def initialize(ring, sprocket)
     @ring, @sprocket = ring, sprocket
@@ -17,10 +19,12 @@ class Gear
   end
   def format_line
     ring_num = Rings.find_index(ring)
-    "#{ring} x #{sprocket} #{" "*Width*ring_num}#{ratio_inches.round(1).to_s.rjust(Width)}"
+    vel = ratio_inches*Math::PI*Rpm*60*CmPerInch/100000
+    #"#{ring} x #{sprocket} #{" "*Width*ring_num}#{ratio_inches.round(1).to_s.rjust(Width)}"
+    "#{vel.round(1).to_s}\t\t#{sprocket.rjust((ring_num+1)*Width).ljust(Rings.size*Width)} #{" "*Width*ring_num}#{ratio_inches.round(1).to_s.rjust(Width)}"
   end
 end
 gears = Rings.map{|r| Sprockets.map{|s| Gear.new(r, s)}}.flatten
 
-puts "#{Sprockets.size}-speed cassette"
+puts "km/h@#{Rpm}rpm\t" + Rings.map {|r| r.rjust(Width) }.join('') + "    #{Sprockets.size}-speed cassette"
 gears.sort{|a, b| a.ratio_inches <=> b.ratio_inches}.reverse.each{|g| puts g.format_line}
